2592aaf5bb052d494c02c1351767775516d72380
acarumran17 <147183267+acarumran17@users.noreply.github.com>
2024-01-04 15:14:49 +0300

    Create fonksiyon.h

---
+++ fonksiyon.h
@@ -0,0 +1,12 @@
+#ifndef fonksiyon_h
+#define fonksiyon_h
+// Fonksiyon tanımları.
+void ana_menu();
+void arac_listesi();
+void arac_kiralama();
+void arac_iade();
+void kiralama_gecmisi();
+void clear();
+void rent_a_car();
+
+#endif // fonksiyon_h
